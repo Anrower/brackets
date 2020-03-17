d938242e90550568577809c4ab269c438fc5b580
Anrower <anrowerdd@gmail.com>
2020-03-17 20:24:52 +0300

    feat: pass some tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let n = 0;
+  let bracket;
+  let OpenBrackets = [];
+  let CloseBrackets = [];
+  for (let i = 0; i < str.length; i++) {
+    bracket = str[i];
+    if (bracket === '(' || bracket === '[' || bracket === "{") {
+      OpenBrackets.push(bracket);
+    } else if (bracket === ')' || bracket === ']' || bracket === "}") {
+      CloseBrackets.push(bracket);
+    }
+  }
+  if (OpenBrackets.length === CloseBrackets.length) {
+    return true;
+  } else {
+    return false;
+  }
 }
